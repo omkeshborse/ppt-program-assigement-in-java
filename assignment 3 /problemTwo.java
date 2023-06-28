ff448a3e514b3bc8185aaaa69096223cfa4ebd24
omkesh borse <118654283+omkeshborse@users.noreply.github.com>
2023-06-28 14:25:13 +0530

    Update problemTwo.java

--- assignment 3 /problemTwo.java
+++ assignment 3 /problemTwo.java
@@ -1,1 +1,76 @@
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FourSum {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        if (nums == null || nums.length < 4) {
+            return result;
+        }
+
+        Arrays.sort(nums);
+
+        int n = nums.length;
+
+        for (int i = 0; i < n - 3; i++) {
+            // Skip duplicates
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            for (int j = i + 1; j < n - 2; j++) {
+                // Skip duplicates
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+
+                int left = j + 1;
+                int right = n - 1;
+
+                while (left < right) {
+                    int currentSum = nums[i] + nums[j] + nums[left] + nums[right];
+
+                    if (currentSum == target) {
+                        result.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+
+                        // Skip duplicates
+                        while (left < right && nums[left] == nums[left + 1]) {
+                            left++;
+                        }
+                        while (left < right && nums[right] == nums[right - 1]) {
+                            right--;
+                        }
+
+                        left++;
+                        right--;
+                    } else if (currentSum < target) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        FourSum solution = new FourSum();
+
+        int[] nums1 = {1, 0, -1, 0, -2, 2};
+        int target1 = 0;
+        List<List<Integer>> result1 = solution.fourSum(nums1, target1);
+        System.out.println(result1);
+        // Output: [[-2, -1, 1, 2], [-2, 0, 0, 2], [-1, 0, 0, 1]]
+
+        int[] nums2 = {2, 2, 2, 2, 2};
+        int target2 = 8;
+        List<List<Integer>> result2 = solution.fourSum(nums2, target2);
+        System.out.println(result2);
+        // Output: [[2, 2, 2, 2]]
+    }
+}
