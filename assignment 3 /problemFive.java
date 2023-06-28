6c89c981f2ddd1aff64e1b5d7bb3683d5c286096
omkesh borse <118654283+omkeshborse@users.noreply.github.com>
2023-06-28 15:24:06 +0530

    Update problemFive.java

--- assignment 3 /problemFive.java
+++ assignment 3 /problemFive.java
@@ -1,1 +1,21 @@
+class problemFive {
+    public static int[] plusOne(int[] digits) {
+          for (int i = digits.length-1; i>= 0; i--) {
+                if (digits[i] < 9) {
+                    digits[i]++ ;
+                    return digits ;
 
+                }else 
+                digits[i] = 0 ;
+            }
+            int[] newNumber = new int[digits.length +1] ;
+            newNumber[0] = 1 ;
+            return newNumber ;
+    }
+  public static void main(String[] args) {
+		int[] digits = {1,2,3} ;
+    int[] result = plusOne(digits) ;
+    for(int i : result ){
+      System.out.print(i);
+	}
+}
